487d564daa2fab4045e9f80217caf1155c505272
FilaCo <dvchizha@edu.hse.ru>
2019-01-18 16:27:48 +0500

    Начало решения задачи 1002 - Телефонные номера

---
+++ Tasks/Task 1002/Source.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	return 0;
+}
