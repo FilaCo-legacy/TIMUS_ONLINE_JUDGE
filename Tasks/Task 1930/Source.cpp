d44708369a73575c288b8ab8e757674da8d11dd2
FilaCo <dvchizha@edu.hse.ru>
2019-02-02 10:48:02 +0500

    Задача решена, вердикт AC 0.312

--- Tasks/Task 1930/Source.cpp
+++ Tasks/Task 1930/Source.cpp
@@ -2,23 +2,26 @@
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
+#include <vector>
+#include <xmmintrin.h>
 
 using namespace std;
 
-const int MAXN = 1e4, MAXM = 1e5, INF = 1e9;
+const int k_maxn = 1e4, k_maxm = 1e5, k_inf = 1e9;
 
-int graph[MAXN][MAXN], dist[MAXN][2];
-bool used[MAXN][2];
+vector <vector <pair <int, int>>> edges;
+int dist[k_maxn][2];
+bool used[k_maxn][2];
 
-void dijkstra(int start_node_, int size_) {
-	for (int i = 0; i < size_; ++i) {
-		dist[i][0] = dist[i][1] = INF;
+void dijkstra(int start_node, int size) {
+	for (int i = 0; i < size; ++i) {
+		dist[i][0] = dist[i][1] = k_inf;
 	}
-	dist[start_node_][0] = dist[start_node_][1] = 0;
+	dist[start_node][0] = dist[start_node][1] = 0;
 
-	for (int i = 0; i < 2*size_; ++i) {
+	for (int i = 0; i < 2 * size; ++i) {
 		int node = -1, trans = -1;
-		for (int j = 0; j < size_; ++j) {
+		for (int j = 0; j < size; ++j) {
 			for (int k = 0; k < 2; ++k) {
 				if (!used[j][k] && (node == -1 || dist[j][k] < dist[node][trans])) {
 					node = j;
@@ -26,16 +29,12 @@ void dijkstra(int start_node_, int size_) {
 				}
 			}
 		}
-		if (dist[node][trans] == INF)
+		if (dist[node][trans] == k_inf)
 			break;
 		used[node][trans] = true;
-		for (int j = 0; j < size_; ++j) {
-			if (graph[node][j] == -1) {
-				dist[j][0] = min(dist[j][0], dist[node][trans] + trans == 1 ? 1 : 0);
-			}
-			else if (graph[node][j] == 1) {
-				dist[j][1] = min(dist[j][1], dist[node][trans] + trans == 1 ? 0 : 1);
-			}
+		for (int j = 0; j < edges[node].size(); ++j){
+			int req_trans = edges[node][j].second, to = edges[node][j].first;
+			dist[to][req_trans] = min(dist[to][req_trans], dist[node][trans] + (trans == req_trans ? 0 : 1));
 		}
 	}
 }
@@ -47,11 +46,13 @@ int main() {
 #endif // ! ONLINE_JUDGE	
 	int n, m, start, end;
 	scanf("%d%d", &n, &m);
+	for (int i = 0; i < n; ++i)
+		edges.emplace_back(vector < pair<int, int>>());
 	for (int i = 0; i < m; ++i) {
 		int fst, snd;
 		scanf("%d%d", &fst, &snd);
-		graph[fst - 1][snd - 1] = 1;
-		graph[snd - 1][fst - 1] = -1;
+		edges[fst - 1].emplace_back(snd - 1, 1);
+		edges[snd - 1].emplace_back(fst - 1, 0);
 	}
 	scanf("%d%d", &start, &end);
 	dijkstra(start - 1, n);
